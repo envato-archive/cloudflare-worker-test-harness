b6f717637bb3b8baac4ecfa9753596332d423de0
Jacob Bednarz <jacob.bednarz@gmail.com>
2018-09-14 16:20:41 +1000

    Match cloudflare ECMAScript builtins
    Adds three restrictions to the ECMAScript helpers
    
    - Disable `eval()` for security reasons
    - Disable `new Function()` for security reasons
    - Don't progress `Date.now()` during execution
    
    It's worth noting the two disables methods *are not bulletproof*. If you _really_ want to work around them you probably can but this is good enough to stop developers accidentally tripping on it.

--- test/ecmascript_builtins.js
+++ test/ecmascript_builtins.js
@@ -3,3 +3,23 @@ require('../src/ecmascript_builtins')
 test('eval() is not accessible', async () => {
   expect(eval("1 + 1")).toBeUndefined()
 })
+
+test('new Function() is not accessible', async () => {
+  var sum = new Function('a', 'b', 'return a + b');
+  expect(sum).not.toBe(Function)
+})
+
+test('WebAssembly is not accessible', async () => {
+  expect(WebAssembly).toBeUndefined()
+})
+
+test('Date.now does not advance in execution', async () => {
+  var start = Date.now()
+
+  // Purposely do something that will hold the execution for a small
+  // amount of time so that the `Date.now()` calls *should* differ.
+  for (i = 0; i < 10000; i++);
+
+  var end = Date.now()
+  expect(start).toEqual(end)
+})
