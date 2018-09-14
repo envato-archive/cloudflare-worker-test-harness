09a1f5e0d31a68f9af330759ae6c9db97ba9500f
Jacob Bednarz <jacob.bednarz@gmail.com>
2018-09-14 16:26:48 +1000

    Pull in webpack config from rec. source

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   target: 'webworker'
+  mode: 'production',
+
+  // Prevent Webpack from getting angry if we bundle a large script
+  performance: {
+    hints: false,
+  },
+
+  // Prevent Webpack from shimming Node features and bloating our Worker scripts
+  node: false
 };
