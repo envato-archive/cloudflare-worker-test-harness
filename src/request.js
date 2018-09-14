bdcc9355022762a2b2614219c752a01fa3759b5c
Jacob Bednarz <jacob.bednarz@gmail.com>
2018-09-14 10:47:17 +1000

    Remove any non-`cf` features from the options
    In the event that someone attempts to pass in a `cf` feature that isn't
    available, we want to remove it from the options. This will help ensure
    that people can't test non-existent functionality in their suite without
    failures.

--- src/request.js
+++ src/request.js
@@ -4,10 +4,29 @@
 // See https://developers.cloudflare.com/workers/reference/cloudflare-features/
 require('whatwg-fetch')
 
+// Available `cf` features. Any keys that are not here and set in `cf`
+// will be discarded.
+const cfFeatures = [
+  "scrapeShield",
+  "polish",
+  "minify",
+  "mirage",
+  "scrapeShield",
+  "apps",
+  "resolveOverride",
+  "cacheKey",
+  "cacheTtl",
+  "cacheTtlByStatus"
+]
+
 const OldRequest = global.Request
 global.Request = class Request extends OldRequest {
   constructor(input, options) {
     super(input, options)
-    this.cf = options && options.cf || {}
+
+    if (options && options.cf) {
+      sanitisedOptions = options.cf.filter(feature => cfFeatures.includes(feature));
+    }
+    this.cf = sanitisedOptions || {}
   }
 }
